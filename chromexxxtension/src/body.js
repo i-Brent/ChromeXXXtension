531b707073529509a6a12bd3d0d3c63974f2b4ce
timodo <timr@edmodo.com>
2018-12-14 13:57:27 -0800

    minor sidebar adjustments + more content

--- chromexxxtension/src/body.js
+++ chromexxxtension/src/body.js
@@ -18,7 +18,33 @@ class Body extends React.Component{
           <p>-Record the organisms name and then research its diet</p>
           <p>-Input the information on the google form like:Name(Diet)</p>
           <p>-You can use the list to update your Food = Chain Diagram as needed</p>
+          <p></p>          <p>
+          A mammal can be a carnivore (meat eater),
+herbivore (plant eater) or omnivore (meat and plant eater).
+By looking at the teeth, eye position and feet you can
+usually identify which of the three it is.
+First lets look at teeth. If the mammal has long, sharp
+canine teeth next to the front (incisor) teeth and also has
+sharp cheek teeth (carnissials) it is a carnivore. The canines
+are for seizing and stabbing prey. The carnassials help cut
+up meat in the mouth.</p><p>
+If the mammal has either blunt canines or no canines,
+and has big flat side and back teeth (premolars and
+molars) it is a herbivore. Herbivores use their molars for
+crushing and grinding plants.</p><p>
+But what about herbivores that do have large front
+teeth? Beavers, for example, have huge front incisors. And
+river hippos have two huge lower canines. Well, beavers use
+their incisors for chewing through bark, branches and even
+trees. And hippos use their canine tusks for defense and
+fighting, not for chewing. But both beavers and hippos have
+large flat molars that they use for grinding up plant matter.
+If the mammal has a variety of all kinds of teeth
+(canines, incisors, premolars and molars) it is an omnivore. It
+has a variety of teeth because it eats a variety of foods,
+both meat and plants.</p>
       </div>
+     
     )
   }
 }
